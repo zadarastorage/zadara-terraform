ec092d133f88e2d4476d72f627c4077dced02178
marco-zadara <38433114+marco-zadara@users.noreply.github.com>
2021-12-21 14:55:12 +0100

    small changes
    update minor stuff

--- loadbalancer/connect.tf
+++ loadbalancer/connect.tf
@@ -1,17 +1,15 @@
 provider "aws" {
     #tenant
     access_key = "${var.accesskey}"
-    secret_key ="${var.secretkey}"
+    secret_key = "${var.secretkey}"
     #zCloud endpoints
     endpoints {
-        ec2 = "https://${var.zcloud_ip}/api/v2/ec2"
-        elb = "https://${var.zcloud_ip}/api/v2/aws/elb"
+        ec2 = "https://${var.zcloud_ip}/api/v2/aws/ec2"
+        elb = "https://${var.zcloud_ip}/api/v2/aws/elbv2"
         }
     insecure = "true"
     skip_metadata_api_check = true
     skip_credentials_validation = true
     skip_requesting_account_id = true
-    # AWS plugin version
-    version = "=2.31.0"
     region = "us-east-1"
   }
--- loadbalancer/loadbalancer.tf
+++ loadbalancer/loadbalancer.tf
@@ -1,34 +1,42 @@
-resource "aws_alb_target_group" "instances" {
+resource "aws_lb_target_group" "instances" {
   name     = "demo-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${var.vpc}"
 }
 
-resource "aws_alb_target_group_attachment" "attach_web_servers" {
-  target_group_arn = aws_alb_target_group.instances.arn
+resource "aws_lb_target_group_attachment" "attach_web_servers" {
+  target_group_arn = aws_lb_target_group.instances.arn
   target_id        = element(aws_instance.server.*.id, count.index)
   port             = 80
   count = var.quantity
 }
-
-resource "aws_alb" "alb" {
+resource "aws_eip" "alb" {
+  vpc = true
+}
+resource "aws_lb" "alb" {
   name               = "demo-elb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.elb.id]
   subnet_mapping {
     subnet_id     = "${var.subnet}"
+
   }
 }
 
-resource "aws_alb_listener" "alb" {
-  load_balancer_arn = aws_alb.alb.arn
+
+resource "aws_lb_listener" "alb" {
+  load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
   type             = "forward"
-  target_group_arn = aws_alb_target_group.instances.arn
+  target_group_arn = aws_lb_target_group.instances.arn
   }
+}
+output "instance_public_ip" {
+  description = "Public IP address of the Loadbalancer"
+  value       =  aws_eip.alb.public_ip
 }
