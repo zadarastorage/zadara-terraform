fae2e698ecc92fb855146f789a14728ef71edf17
Marco Schneider <38433114+marco-zadara@users.noreply.github.com>
2024-03-19 17:52:22 +0100

    Update connect.tf
    delete insecure setting

--- Basic-server/connect.tf
+++ Basic-server/connect.tf
@@ -6,7 +6,6 @@ provider "aws" {
     endpoints {
         ec2 = "https://${var.zcloud_ip}/api/v2/aws/ec2"
         }
-    insecure = "true"
     skip_metadata_api_check = true
     skip_credentials_validation = true
     skip_requesting_account_id = true
