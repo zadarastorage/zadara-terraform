c8fe4df91b913fa26aef3032ecd2b8582528506f
Jeff Alyanak <jeff@alyanak.ca>
2024-07-16 15:23:45 +0100

    Cleaned up formatting

--- Basic-server/connect.tf
+++ Basic-server/connect.tf
@@ -6,18 +6,19 @@ terraform {
     }
   }
 }
+
 provider "aws" {
     #tenant
-    access_key = "${var.accesskey}"
-    secret_key = "${var.secretkey}"
+    access_key = var.accesskey
+    secret_key = var.secretkey
     #zCloud endpoints
     endpoints {
         ec2 = "https://${var.zcloud_ip}/api/v2/ec2"
         iam = "https://${var.zcloud_ip}/api/v2/aws/iam"
     }
-    insecure = "true"
-    skip_metadata_api_check = true
+    insecure                    = "true"
+    skip_metadata_api_check     = true
     skip_credentials_validation = true
-    skip_requesting_account_id = true
-    region = "us-east-1"
+    skip_requesting_account_id  = true
+    region                      = "us-east-1"
 }
